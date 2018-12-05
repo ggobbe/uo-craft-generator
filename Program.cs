aafe457bc7f9f957ee6f3f2d9332877e385070c6
ggobbe <guillaume.gobbe@gmail.com>
2018-12-05 13:54:20 +0100

    Deal with female and man only items

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Text;
 using CsvHelper;
 using CsvHelper.Configuration;
@@ -30,13 +32,28 @@ namespace item_generator
                     {
                         var tag = new StringBuilder().Append(TagPrefix).Append(key).Append(TagSuffix);
                         var value = csv[key];
+
+                        if (key.StartsWith("if:"))
+                        {
+                            value = bool.Parse(value) ? string.Empty : "//";
+                        }
+
                         output = output.Replace(tag.ToString(), value);
                     }
 
-                    output.Append("\n\n");
-                    swOutput.Write(output);
+                    output.Append(Environment.NewLine);
+                    output.Append(Environment.NewLine);
+
+                    var toWrite = RemoveComments(output.ToString());
+                    swOutput.Write(toWrite);
                 }
             }
+        }
+
+        private static string RemoveComments(string input)
+        {
+            var lines = input.Split(Environment.NewLine).Where(l => !l.TrimStart().StartsWith("//"));
+            return string.Join(Environment.NewLine, lines);
         }
     }
 }
