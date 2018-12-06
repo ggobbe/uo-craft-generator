a9de3759558351ec30dfcc1efdce3013ccb152fa
ggobbe <guillaume.gobbe@gmail.com>
2018-12-06 13:52:22 +0100

    Add def generation

--- Program.cs
+++ Program.cs
@@ -14,12 +14,18 @@ namespace item_generator
 
         static void Main(string[] args)
         {
-            using (var srItems = new StreamReader("items.csv"))
-            using (var srTemplate = new StreamReader("template.txt"))
+            GenerateCode("items.csv", "template-item.txt", "output-items.txt");
+            GenerateCode("items.csv", "template-def.txt", "output-def.txt");
+        }
+
+        private static void GenerateCode(string csvFile, string templateFile, string outputFile)
+        {
+            using (var srItems = new StreamReader(csvFile))
+            using (var template = new StreamReader(templateFile))
             using (var csv = new CsvReader(srItems, new Configuration {Delimiter = ","}))
-            using (var swOutput = new StreamWriter("output.txt"))
+            using (var output = new StreamWriter(outputFile))
             {
-                var templateText = srTemplate.ReadToEnd();
+                var itemTemplateCode = template.ReadToEnd();
 
                 csv.Read();
                 csv.ReadHeader();
@@ -27,10 +33,11 @@ namespace item_generator
 
                 while (csv.Read())
                 {
-                    var output = new StringBuilder(templateText);
+                    var itemCode = new StringBuilder(itemTemplateCode);
+
                     foreach (var key in headerRow)
                     {
-                        var tag = new StringBuilder().Append(TagPrefix).Append(key).Append(TagSuffix);
+                        var tag = new StringBuilder().Append(TagPrefix).Append(key).Append(TagSuffix).ToString();
                         var value = csv[key];
 
                         if (key.StartsWith("if:"))
@@ -38,21 +45,23 @@ namespace item_generator
                             value = bool.Parse(value) ? string.Empty : "//";
                         }
 
-                        output = output.Replace(tag.ToString(), value);
+                        if (key.StartsWith("switch:"))
+                        {
+                            tag = new StringBuilder().Append(TagPrefix).Append("switch:").Append(value).Append(TagSuffix).ToString();
+                            value = string.Empty;
+                        }
+
+                        itemCode = itemCode.Replace(tag, value);
                     }
 
-                    output.Append(Environment.NewLine);
-                    output.Append(Environment.NewLine);
-
-                    var toWrite = RemoveComments(output.ToString());
-                    swOutput.Write(toWrite);
+                    output.Write(RemoveComments(itemCode.ToString()));
                 }
             }
         }
 
         private static string RemoveComments(string input)
         {
-            var lines = input.Split(Environment.NewLine).Where(l => !l.TrimStart().StartsWith("//"));
+            var lines = input.Split(Environment.NewLine).Where(l => !l.TrimStart().StartsWith("//") && !l.TrimStart().StartsWith("{{switch:"));
             return string.Join(Environment.NewLine, lines);
         }
     }
